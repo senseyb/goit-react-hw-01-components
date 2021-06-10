a54c3d753fc7cf12210e93a889012942c92514f9
senseyb <senseyb7@gmail.com>
2021-06-10 20:37:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 
+
 ReactDOM.render(
   <>
-    <App />
+    <App/>
   </>,
   document.getElementById("root")
 );
